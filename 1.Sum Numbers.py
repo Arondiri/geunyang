e3f07120f16d4059e36eb681ae996910d6b219d0
Arondiri <67238043+Arondiri@users.noreply.github.com>
2020-07-04 22:40:45 +0900

    Create 1.Sum Numbers.py

---
+++ 1.Sum Numbers.py
@@ -0,0 +1,27 @@
+def sum_numbers(text: str) -> int:
+    A = list(text)
+    B = [[],[]]
+    c = 0
+    if len(A) == 1 or len(A) == 0:
+        return 0
+    else:
+        if A[0].isdigit() and (A[1].isdigit() or A[1] == " "):
+            B[0].append(A[0])
+            B[1].append(0)
+        if A[-1].isdigit() and (A[-2].isdigit() or A[-2] == " "):
+            B[0].append(A[-1])
+            B[1].append(len(A)-1)
+        for i in range(1,len(A)-1):
+            if A[i].isdigit() and (A[i+1].isdigit() or A[i+1] == " ") and (A[i-1].isdigit() or A[i-1] == " "):
+                B[0].append(A[i])
+                B[1].append(i)
+        C = B[1][:-1]
+        D = B[0]
+        for i in C:
+            if i in B[1] and i+1 in B[1]:
+                D[B[1].index(i)+1] = D[B[1].index(i)]+D[B[1].index(i)+1]
+                D.pop(B[1].index(i))
+                B[1].remove(i)
+        for i in B[0]:
+            c += int(i)
+        return c
