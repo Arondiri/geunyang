07c6eb8b1dcbd364e660439616f035ecf8dbd7f2
Arondiri <67238043+Arondiri@users.noreply.github.com>
2020-07-04 22:33:22 +0900

    Delete 6.End Zeros.py

--- 6.End Zeros.py
+++
@@ -1,9 +0,0 @@
-def end_zeros(num: int) -> int:
-    b = 0
-    if num == 0:
-        return 1
-    else:
-        while num%10 == 0:
-            num = num//10
-            b += 1
-        return b
