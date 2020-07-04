909787c8b6c77044b9e16413ba82ef644de5a7cc
Arondiri <67238043+Arondiri@users.noreply.github.com>
2020-07-04 14:41:55 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -67,3 +67,20 @@ def left_join(phrases: tuple) -> str:
     return B
 
 #%%
+def first_word(text: str) -> str:
+    a = 0
+    b = 0
+    for i in range(len(text)):
+        if text[i] != " " and text[i] != "," and text[i] != ".":
+            a = i
+            break
+    if ' ' in text[a:] or ',' in text[a:] or '.' in text[a:]:
+        for i in range(a,len(text)):
+            if text[i] == " " or text[i] == "," or text [i] == ".":
+                b = i
+                break
+        return text[a:b]
+    else:
+        return text[a:]
+
+#%%
