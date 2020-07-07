c37d015c3e1936d348b540228d1195edc897e6b8
Arondiri <67238043+Arondiri@users.noreply.github.com>
2020-07-08 07:31:27 +0900

    Create 16.Sun Angle.py

---
+++ 16.Sun Angle.py
@@ -0,0 +1,7 @@
+time = time.split(':')
+    for i in range(len(time)):
+        time[i] = int(time[i])
+    if time[0] < 6 or (time[0] >= 18 and time[1] > 0):
+        return "I don't see the sun!"
+    else:
+        return ((((time[0]-6)*60+time[1])*18000)//(12*60))/100
