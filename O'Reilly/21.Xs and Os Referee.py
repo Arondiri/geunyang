fd27517884d56c6b2f9beaff67e3a70b39d645f3
Arondiri <67238043+Arondiri@users.noreply.github.com>
2020-07-25 19:11:45 +0900

    Update 21.Xs and Os Referee.py

--- O'Reilly/21.Xs and Os Referee.py
+++ O'Reilly/21.Xs and Os Referee.py
@@ -1,1 +1,14 @@
+from typing import List
 
+def checkio(game_result: List[str]) -> str:
+    for i in range(3):
+        if game_result[0][i] == game_result[1][i] == game_result[2][i] == 'O':
+            return 'O'
+        elif game_result[0][i] == game_result[1][i] == game_result[2][i] == 'X':
+            return 'X'
+    if 'OOO' in game_result or game_result[0][0] == game_result[1][1] == game_result[2][2] == 'O' or game_result[0][2] == game_result[1][1] == game_result[2][0] == 'O':
+        return 'O'
+    elif 'XXX' in game_result or game_result[0][0] == game_result[1][1] == game_result[2][2] == 'X' or game_result[0][2] == game_result[1][1] == game_result[2][0] == 'X':
+        return 'X'
+    else:
+        return 'D'
